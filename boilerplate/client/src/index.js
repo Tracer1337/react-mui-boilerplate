937deafc4edbf985559ae5a191da9f5d43fc513b
Merlin Moelter <joebau.0815@googlemail.com>
2020-08-12 08:52:08 +0200

    Add default code to client/index.js

--- boilerplate/client/src/index.js
+++ boilerplate/client/src/index.js
@@ -1,18 +1,50 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import { Provider } from "react-redux"
+import { createMuiTheme, ThemeProvider } from "@material-ui/core"
 
+import store from "./store"
 import App from "./App.js"
 import * as serviceWorker from "./serviceWorker.js"
 import "./index.css"
 
+const theme = createMuiTheme({
+    palette: {
+        theme: "dark",
+        type: "dark",
+
+        background: {
+            default: "#282833",
+            paper: "#333340"
+        },
+
+        primary: {
+            main: "#BB86FC",
+            variant: "#3700B3"
+        },
+
+        secondary: {
+            main: "#03DAC5",
+            dark: "#336D6F"
+        },
+
+        error: {
+            main: "#CF6679"
+        }
+    }
+})
+
+if (process.env.NODE_ENV === "development") {
+    console.log(theme)
+}
+
 ReactDOM.render(
-    <React.StrictMode>
-        <App/>
-    </React.StrictMode>,
-    document.getElementById("root")
+    <Provider store={store}>
+        <ThemeProvider theme={theme}>
+            <App />
+        </ThemeProvider>
+    </Provider>,
+    document.getElementById('root')
 )
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister()
